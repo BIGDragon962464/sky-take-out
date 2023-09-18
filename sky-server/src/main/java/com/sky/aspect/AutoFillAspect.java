ef4fcde2d30c8276626aa03e225bf9c6d1ded67f
caozl <cao.zl@outlook.com>
2023-09-18 16:19:12 +0800

    自动填充（未完成）

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,50 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义切面，实现公共字段自动填充处理逻辑
+ */
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){
+
+    }
+
+    /**
+     * 自定义前置通知，在通知中进行公共字段的赋值
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        log.info("开始进行公共字段的自动填充...");
+
+        //获取到当前被拦截的方法上的数据库操作类型
+        MethodSignature signature =(MethodSignature) joinPoint.getSignature();//方法签名对象
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);//获得方法上的注解对象
+        OperationType operationType = autoFill.value();//获得数据库操作类型
+
+        //获取到当前被拦截的方法的参数--实体对象
+        Object[] args = joinPoint.getArgs();
+
+        //准备赋值的数据
+
+        //根据当前不同的操作类型，为对应的属性通过反射来赋值
+    }
+}
